c34103ba193d2658bb0582b1a1c61d5ad45e7122
Daniel O'Donnell <daniel.t.odonnell@gmail.com>
2019-02-03 16:15:23 -0800

    Moved auth into header

--- api.js
+++ api.js
@@ -1,13 +1,15 @@
 const axios = require('axios')
 const querystring = require('querystring')
+const url = 'https://api.airtable.com/v0'
+const key = process.env.AIRTABLE_KEY
+axios.defaults.headers.common['Authorization'] = `Bearer ${key}`
 
 module.exports = (req, res) => {
-  const url = 'https://api.airtable.com/v0'
+
   const qs = querystring.parse(req.url.split('?')[1])
-  const key = process.env.AIRTABLE_KEY
   const base = qs.base || 'NO_BASE_SPECIFIED'
   const tab = qs.tab || 'NO_TAB_SPECIFIED'
-  const api = `${url}/${base}/${tab}?api_key=${key}`
+  const api = `${url}/${base}/${tab}`
 
   console.log(api, qs, url, base, tab, key)
 
